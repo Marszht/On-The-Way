7e56a4f040215850f12b58bb2b6aeb003d83180a
Marszht <1046209799@qq.com>
2018-09-28 13:08:15 +0800

    deleted

--- TV/pages/videos/video-detail/video-detail.js
+++ TV/pages/videos/video-detail/video-detail.js
@@ -1,5 +1,5 @@
 // pages/videos/video-detail/video-detail.js
-const util = require ('../../../utils/util.js');
+const util = require('../../../utils/util.js');
 const app = getApp();
 const movieUrl = app.globalData.movieBase;
 Page({
@@ -8,8 +8,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-     video: {},
-     movieList: []
+    video: {},
+    movieList: []
   },
 
   /**
@@ -18,42 +18,45 @@ Page({
   onLoad: function (options) {
     // options 是url 里面传过来的值
     //  console.log( options );
-     this.setData({
-        video:  {
-           time: decodeURIComponent(options.time),
-           url: decodeURIComponent(options.url),
-           title: decodeURIComponent(options.title)
-        }
-     })
-     wx.setNavigationBarTitle({
-        title: decodeURIComponent(options.title),
-     })
-     this._getMovieList();
-   //  console.log( this.data.video )
+    this.setData({
+      video: {
+        time: decodeURIComponent(options.time),
+        url: decodeURIComponent(options.url),
+        title: decodeURIComponent(options.title)
+      }
+    })
+    wx.setNavigationBarTitle({
+      title: decodeURIComponent(options.title),
+    })
+    this._getMovieList();
+
 
 
   },
+
+
   //  获取推荐影片
-_getMovieList () {
-  util.$get(`${movieUrl}/api/v2/article`, {
-    app_id: 6,
-    cid: 4,
-    article_id: 0
-  }).then( res => {
-    let status = res.data.status;
-    if (status === 0 ) {
-      //  对获取数据进行处理
-      res.data.data.articles.map(v => { // 转换一下时间
-        v.create_time = util.formatTime(new Date(), 'yyyy-MM-dd')
-        //  正则？
-        v.thumbnails[0].url = v.thumbnails[0].url.replace(/(\.\w{3,4})$/i, "_crop" + 234 + "x" + 146 + "$1")
-      })
-      this.setData({
-        movieList: res.data.data.articles
-      }) 
-    }
-  })
-},
+  _getMovieList() {
+    util.$get(`${movieUrl}/api/v2/article`, {
+      app_id: 6,
+      cid: 4,
+      article_id: 0
+    }).then(res => {
+      let status = res.data.status;
+      if (status === 0) {
+        //  对获取数据进行处理
+        res.data.data.articles.map(v => { // 转换一下时间
+          v.create_time = util.formatTime(new Date(), 'yyyy-MM-dd')
+          //  正则？
+          v.thumbnails[0].url = v.thumbnails[0].url.replace(/(\.\w{3,4})$/i, "_crop" + 234 + "x" + 146 + "$1")
+        })
+        this.setData({
+          movieList: res.data.data.articles
+        })
+        console.log("movie:", this.data.movieList)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -101,5 +104,14 @@ _getMovieList () {
    */
   onShareAppMessage: function () {
 
+  },
+  //  打开推荐重新渲染detail一遍
+  openDetail(e) {
+    let item =  e.currentTarget.dataset.list;
+    let url = `../video-detail/video-detail?title=${item.title}&time=${item.create_time}&url=${item.videos[0].video_src}`
+    wx.redirectTo({
+      url: url
+    })
+    console.log(e)
   }
 })
--- 王冰文化/王冰文化小程序/api/url.js
+++
@@ -1,8 +0,0 @@
-// export const HOST = 'http://192.168.0.199/'
-export const HOST = 'https://wangbingwenhuaapi2.hzyc8.top/'
-// 自定义登录态
-export const AUTH = HOST + 'miniapp/login' 
-export const ASK = HOST + 'miniapp/question/ask'
-export const GETASK = HOST + 'miniapp/question/own'
-export const INFO = HOST + 'miniapp/info'
-export const SEARCH = HOST + 'miniapp/question'
